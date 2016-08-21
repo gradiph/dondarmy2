7cd3c54bebd177cda75a9b4b13fd93d6f25a670f
Gradi <gradiph@gmail.com>
2016-08-21 09:52:17 +0700

    210816 menambahkan field role

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'nama', 'password',
+        'username', 'nama', 'password', 'role',
     ];
 
     /**
